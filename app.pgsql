20358ba44f04da9aaa21d02646b3e6e472c21628
Michal Migurski <mike@teczno.com>
2017-02-14 12:11:50 -0800

    Added single-feature Postgres table to verify database setup

---
+++ app.pgsql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS world;
+DROP EXTENSION IF EXISTS postgis;
+
+CREATE EXTENSION postgis;
+
+CREATE TABLE world
+(
+);
+
+SELECT AddGeometryColumn('world', 'geom', 4326, 'POLYGON', 2);
+
+INSERT INTO world (geom) VALUES (ST_SetSRID(ST_MakeBox2D(ST_MakePoint(-180, -90), ST_MakePoint(180, 90)), 4326));
--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-import flask
+import flask, psycopg2, os, json
 
 app = flask.Flask(__name__)
 
 @app.route('/')
 def get_index():
-    return 'Hello World'
+    with psycopg2.connect(os.environ['DATABASE_URL']) as conn:
+        with conn.cursor() as db:
+            db.execute('SELECT ST_AsGeoJSON(geom) FROM world LIMIT 1')
+            (geometry, ) = db.fetchone()
+    
+    feature = dict(geometry=json.loads(geometry), type='Feature', properties={})
+    geojson = dict(type='FeatureCollection', features=[feature])
+    
+    return flask.jsonify(geojson)
 
 if __name__ == '__main__':
     app.run(debug=True)
