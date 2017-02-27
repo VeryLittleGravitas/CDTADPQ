50ee8c0d727353ab8b7d073ea9a3d121ed7e5b09
Erica Kwan <erica.kwan@gmail.com>
2017-02-27 14:49:25 -0800

    Can send fire notifications to users

---
+++ CDTADPQ/data/notify.py
@@ -0,0 +1,81 @@
+import codecs, json, requests, uritemplate
+import os, psycopg2, psycopg2.extras
+
+
+from . import wildfires
+from . import zipcodes
+from . import users
+
+def main():
+    '''
+    '''
+    if os.environ['TWILIO_ACCOUNT'].startswith('AC'):
+        # TODO move TwilioAccount out of users
+        twilio_account = users.TwilioAccount(
+            sid = os.environ.get('TWILIO_SID', ''),
+            secret = os.environ.get('TWILIO_SECRET', ''),
+            account = os.environ.get('TWILIO_ACCOUNT', ''),
+            number = os.environ.get('TWILIO_NUMBER', '')
+            )
+    else:
+        twilio_account = users.TwilioAccount(
+            sid = codecs.decode(os.environ.get('TWILIO_SID', ''), 'rot13'),
+            secret = codecs.decode(os.environ.get('TWILIO_SECRET', ''), 'rot13'),
+            account = codecs.decode(os.environ.get('TWILIO_ACCOUNT', ''), 'rot13'),
+            number = os.environ.get('TWILIO_NUMBER', '')
+            )
+
+    with psycopg2.connect(os.environ['DATABASE_URL']) as conn:
+        with conn.cursor(cursor_factory = psycopg2.extras.DictCursor) as db:
+            fires = get_all_wild_fires(db)
+            for fire in fires:
+                user_rows = get_users_to_notify(db, fire)
+                for user_row in user_rows:
+                    send_notification(twilio_account, user_row['phone_number'], fire)
+
+def get_all_wild_fires(db):
+    ''' Return all fires that users should be notified of
+    '''
+    # Need to not get all fires, need to get all for today or something?
+    db.execute('SELECT ST_AsGeoJSON(location) as coordinates_json, * FROM fire_points')
+    fire_rows = db.fetchall()
+    fires = []
+    for fire_row in fire_rows:
+        fire_point = wildfires.FirePoint(fire_row['coordinates_json'], fire_row['usgs_id'], fire_row['name'],
+                                         fire_row['contained'], fire_row['discovered'], fire_row['cause'],
+                                         fire_row['acres'])
+        fires.append(fire_point)
+    return fires
+
+def get_users_to_notify(db, fire_point):
+    ''' Return the users that should be notified of this fire
+    '''
+    fire_coordinates = json.loads(fire_point.location)['coordinates']
+    fire_zipcode = zipcodes.lookup_zipcode(db, fire_coordinates[1], fire_coordinates[0])
+    db.execute('SELECT * FROM users WHERE %s=ANY(zip_codes)', (fire_zipcode, ))
+    user_rows = db.fetchall()
+    return user_rows
+
+TwilioURL = 'https://api.twilio.com/2010-04-01/Accounts/{account}/Messages.json'
+
+def send_notification(account, to_number, fire):
+    ''' Send fire notification to phone number
+    '''
+    url = uritemplate.expand(TwilioURL, dict(account=account.account))
+    body = 'Fire {}'.format(fire.name)
+    data = dict(From=account.number, To=to_number, Body=body)
+    auth = account.sid, account.secret
+    posted = requests.post(url, auth=auth, data=data)
+    
+    if posted.status_code not in range(200, 299):
+        if 'message' in posted.json():
+            raise RuntimeError(posted.json()['message'])
+        else:
+            raise RuntimeError('Bad response from Twilio')
+    
+    return True
+
+
+if __name__ == '__main__':
+    exit(main())
+
---
+++ CDTADPQ/data/test_notify.py
@@ -0,0 +1,82 @@
+import os, unittest, unittest.mock, httmock, urllib.parse
+from . import recreate
+from . import notify
+from . import users
+from . import wildfires
+
+import psycopg2, psycopg2.extras
+
+class NotifyTests (unittest.TestCase):
+    def setUp(self):
+        '''
+        '''
+        self.database_url = os.environ['DATABASE_URL']
+        recreate.main(self.database_url)
+
+        with psycopg2.connect(self.database_url) as conn:
+            with conn.cursor(cursor_factory = psycopg2.extras.DictCursor) as db:
+                fire = wildfires.convert_fire_point({'type': 'Feature', 'geometry': {'type': 'Point', 'coordinates': [-122, 37]},
+                                                     'properties': {'objectid': 1, 'latitude': 37, 'longitude': -122, 'gacc': 'SACC',
+                                                     'hotlink': 'http://www.nifc.gov/fireInfo/nfn.htm', 'state': 'OK', 'status': 'I', 'irwinid': '{14DD44C2-ACF0-4998-A99B-971BF60E9A11}',
+                                                     'acres': 1500, 'firecause': 'Human', 'reportdatetime': 1487091600000, 'percentcontained': 49, 'uniquefireidentifier': '2017-OKNEU-170102',
+                                                     'firediscoverydatetime': 1486984500000, 'complexparentirwinid': None, 'pooresponsibleunit': 'OKNEU', 'incidentname': 'Spike Road',
+                                                     'iscomplex': 'false', 'irwinmodifiedon': 1487067267000, 'mapsymbol': '1', 'datecurrent': 1487235965000, 'pooownerunit': None,
+                                                     'owneragency': None, 'fireyear': None, 'localincidentidentifier': None, 'incidenttypecategory': None}})
+                wildfires.store_fire_point(db, fire)
+                db.execute('INSERT INTO users (phone_number, zip_codes, email_address) VALUES (%s, %s, %s)',
+                           ('+15105551212', ['95065'], 'user1@example.com'))
+                db.execute('INSERT INTO users (phone_number, zip_codes, email_address) VALUES (%s, %s, %s)',
+                           ('+15103351786', ['94103'], 'user2@example.com'))
+
+
+    def test_main(self):
+        '''
+        '''
+        with unittest.mock.patch('CDTADPQ.data.notify.get_all_wild_fires') as get_all_wild_fires, \
+             unittest.mock.patch('CDTADPQ.data.notify.get_users_to_notify') as get_users_to_notify, \
+             unittest.mock.patch('CDTADPQ.data.notify.send_notification') as send_notification:
+            get_all_wild_fires.return_value = [wildfires.FirePoint('location', '123', 'fire', 'True', 'now', 'people', 15)]
+            get_users_to_notify.return_value = [{'phone_number': '+15105551212'}]
+            notify.main()
+        self.assertEqual(len(get_all_wild_fires.mock_calls), 1)
+        self.assertEqual(len(get_users_to_notify.mock_calls), 1)
+        self.assertEqual(len(send_notification.mock_calls), 1)
+
+    def test_get_all_wild_fires(self):
+        '''
+        '''
+        with psycopg2.connect(self.database_url) as conn:
+            with conn.cursor(cursor_factory = psycopg2.extras.DictCursor) as db:
+                fires = notify.get_all_wild_fires(db)
+                self.assertEqual(1, len(fires))
+
+    def test_get_users_to_notify(self):
+        with psycopg2.connect(self.database_url) as conn:
+            with conn.cursor(cursor_factory = psycopg2.extras.DictCursor) as db:
+                fires = notify.get_all_wild_fires(db)
+                users = notify.get_users_to_notify(db, fires[0])
+                self.assertEqual(1, len(users))
+                self.assertEqual('+15105551212', users[0]['phone_number'])
+
+    def test_send_notification(self):
+        account = users.TwilioAccount('sid', 'secret', 'account', 'number')
+        fire = unittest.mock.Mock(name='Bad Fire')
+
+        def response_content_error(url, request):
+            if (request.method, url.hostname, url.path) != ('POST', 'api.twilio.com', '/2010-04-01/Accounts/account/Messages.json'):
+                raise Exception('Nope')
+
+            if request.headers['Authorization'] != 'Basic c2lkOnNlY3JldA==':
+                return httmock.response(401, b'Go away')
+            
+            body = 'Fire {}'.format(fire.name)
+            form = dict(urllib.parse.parse_qsl(request.body))
+            
+            if form == {'From': 'number', 'To': '+15105551212', 'Body': body}:
+                body = '''{"sid": "...", "date_created": "Wed, 22 Feb 2017 02:32:26 +0000", "date_updated": "Wed, 22 Feb 2017 02:32:26 +0000", "date_sent": null, "account_sid": "...", "to": "+15105551212", "from": "+15105551212", "messaging_service_sid": null, "body": "Yo", "status": "queued", "num_segments": "1", "num_media": "0", "direction": "outbound-api", "api_version": "2010-04-01", "price": null, "price_unit": "USD", "error_code": null, "error_message": null, "uri": "/2010-04-01/Accounts/.../Messages/....json", "subresource_uris": {"media": "/2010-04-01/Accounts/.../Messages/.../Media.json"}}'''
+                return httmock.response(201, body.encode('utf8'), {'Content-Type': 'application/json'})
+
+            raise Exception('Nope')
+        
+        with httmock.HTTMock(response_content_error):
+            notify.send_notification(account, '+15105551212', fire)
