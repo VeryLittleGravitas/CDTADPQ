26d76f74ac9e1d2c39778380245458c5bd845d35
Michal Migurski <mike@teczno.com>
2017-02-28 19:45:29 -0800

    Added Earthquakes to collection script

--- CDTADPQ/data/collect.py
+++ CDTADPQ/data/collect.py
@@ -1,9 +1,11 @@
 from . import wildfires
+from . import earthquakes
 
 def main():
     '''
     '''
     wildfires.main()
+    earthquakes.main()
 
 if __name__ == '__main__':
     exit(main())
--- CDTADPQ/data/test_collect.py
+++ CDTADPQ/data/test_collect.py
@@ -6,7 +6,9 @@ class CollectTests (unittest.TestCase):
     def test_main(self):
         '''
         '''
-        with unittest.mock.patch('CDTADPQ.data.wildfires.main') as wildfires:
+        with unittest.mock.patch('CDTADPQ.data.wildfires.main') as wildfires, \
+             unittest.mock.patch('CDTADPQ.data.earthquakes.main') as earthquakes:
             collect.main()
         
         wildfires.assert_called_once_with()
+        earthquakes.assert_called_once_with()
