b2027ae6101aa4a1d544fd8317bcb93654b0b643
Michal Migurski <mike@teczno.com>
2017-03-02 19:09:32 -0800

    Added test for notifications.send_sms

--- CDTADPQ/data/test_notify.py
+++ CDTADPQ/data/test_notify.py
@@ -87,23 +87,9 @@ class NotifyTests (unittest.TestCase):
         fire = unittest.mock.Mock(name='Bad Fire')
         test_user = users.User(1, '+15105551212', ['94107'], 'test_user@example.com', ['fire'])
 
-        def response_content_error(url, request):
-            if (request.method, url.hostname, url.path) != ('POST', 'api.twilio.com', '/2010-04-01/Accounts/account/Messages.json'):
-                raise Exception('Nope')
-
-            if request.headers['Authorization'] != 'Basic c2lkOnNlY3JldA==':
-                return httmock.response(401, b'Go away')
-            
-            form = dict(urllib.parse.parse_qsl(request.body))
-            
-            if form == {'From': 'number', 'To': '+15105551212', 'Body': 'omg fire'}:
-                body = '''{"sid": "...", "date_created": "Wed, 22 Feb 2017 02:32:26 +0000", "date_updated": "Wed, 22 Feb 2017 02:32:26 +0000", "date_sent": null, "account_sid": "...", "to": "+15105551212", "from": "+15105551212", "messaging_service_sid": null, "body": "Yo", "status": "queued", "num_segments": "1", "num_media": "0", "direction": "outbound-api", "api_version": "2010-04-01", "price": null, "price_unit": "USD", "error_code": null, "error_message": null, "uri": "/2010-04-01/Accounts/.../Messages/....json", "subresource_uris": {"media": "/2010-04-01/Accounts/.../Messages/.../Media.json"}}'''
-                return httmock.response(201, body.encode('utf8'), {'Content-Type': 'application/json'})
-
-            raise Exception('Nope')
-        
-        with httmock.HTTMock(response_content_error):
+        with unittest.mock.patch('CDTADPQ.data.notifications.send_sms') as send_sms:
             notify.send_notification(account, test_user, 'omg fire')
+            send_sms.assert_called_once_with(account, test_user.phone_number, 'omg fire')
 
     def test_log_user_notification(self):
         '''
--- test.py
+++ test.py
@@ -5,6 +5,7 @@ from CDTADPQ.web.test_init import AppTests
 from CDTADPQ.data.test_users import UsersTests
 from CDTADPQ.data.test_collect import CollectTests
 from CDTADPQ.data.test_notify import NotifyTests
+from CDTADPQ.data.test_notifications import NotificationsTests
 from CDTADPQ.data.test_wildfires import WildfireTests
 from CDTADPQ.data.test_earthquakes import EarthquakeTests
 from CDTADPQ.data.test_floods import FloodTests
